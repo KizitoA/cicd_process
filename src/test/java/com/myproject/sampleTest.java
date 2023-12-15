a08901a666ce1cca70a0d6b5a588d53f3c995813
Amungwa <amungwak@gmail.com>
2023-12-15 12:17:50 +0100

    precaution

---
+++ src/test/java/com/myproject/sampleTest.java
@@ -0,0 +1,13 @@
+package com.myproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class sampleTest 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
